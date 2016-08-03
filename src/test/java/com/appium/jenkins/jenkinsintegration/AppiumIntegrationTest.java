2c989482a3b03ba4635509f35c80c3f8bc121830
rohit <rohit@Rohit-MacBook-Pro.local>
2016-08-03 17:52:09 +0530

    Adding Changes

--- src/test/java/com/appium/jenkins/jenkinsintegration/AppiumIntegrationTest.java
+++ src/test/java/com/appium/jenkins/jenkinsintegration/AppiumIntegrationTest.java
@@ -7,8 +7,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.ExecuteException;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,31 +22,21 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class AppiumIntegrationTest {
-	
-	@BeforeClass
-	public void startAppiumServer() throws ExecuteException, IOException, InterruptedException{
-		new AppiumServer().startAppium();
-	}
 
 	@Test
-		public void testLaunch() throws InterruptedException, IOException {
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
-			cap.setCapability(MobileCapabilityType.PLATFORM_NAME, System.getProperty("PlatformName"));
-			cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, System.getProperty("PlatformVersion"));
-			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
-			cap.setCapability(MobileCapabilityType.APP, "/Users/rohit/Desktop/Consumer.apk");
-			cap.setCapability("app_package", "com.driveu.customer");
-			cap.setCapability("app_activity", "com.driveu.customer.activity.ConfigScreenActivity");
-			AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			new AppiumServer().adbPermisions();
-			Thread.sleep(20000);
-		}
-	
-	@AfterClass
-	public void stopAppiumServer() throws IOException{
-		new AppiumServer().stopAppium();
-	}
+	public void testLaunch() throws InterruptedException, IOException {
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, System.getProperty("PlatformName"));
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, System.getProperty("PlatformVersion"));
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+		cap.setCapability(MobileCapabilityType.APP, "/Users/rohit/Desktop/Consumer.apk");
+		cap.setCapability("app_package", "com.driveu.customer");
+		cap.setCapability("app_activity", "com.driveu.customer.activity.ConfigScreenActivity");
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		new AppiumServer().adbPermisions();
+		Thread.sleep(20000);
 	}
 
+}
